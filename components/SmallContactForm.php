e63c7501b9c44d8dea17e28182df2f37fb2667a0
Jan Vince <jan@vince.cz>
2019-02-13 20:11:02 +0100

    When placeholders are used, labels are now still present and only hidden by style attribute

--- components/SmallContactForm.php
+++ components/SmallContactForm.php
@@ -358,8 +358,8 @@ class SmallContactForm extends ComponentBase
       }
 
       // Label classic
-      if( !empty($fieldSettings['label']) and !Settings::getTranslated('form_use_placeholders') and !empty($fieldType['label']) ){
-        $output[] = '<label class="control-label ' . ( !empty($fieldSettings['label_css']) ? $fieldSettings['label_css'] : '' ) . ' ' . ( $fieldRequired ? 'required' : '' ) . '" for="' . ($this->alias . '-' . $fieldSettings['name']) . '">' . Settings::getDictionaryTranslated($fieldSettings['label']) . '</label>';
+      if( !empty($fieldSettings['label']) and !empty($fieldType['label']) ){
+        $output[] = '<label class="control-label ' . ( !empty($fieldSettings['label_css']) ? $fieldSettings['label_css'] : '' ) . ' ' . ( $fieldRequired ? 'required' : '' ) . '" for="' . ($this->alias . '-' . $fieldSettings['name']) . '" ' . (Settings::getTranslated('form_use_placeholders') ? ' style="display: none;" ' : '') . '>' . Settings::getDictionaryTranslated($fieldSettings['label']) . '</label>';
       }
 
       // Label as container
